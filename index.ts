3177f7745b86316dfc018179f6d1687288cc5ee4
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-14 23:51:22 +0530

    todo

--- index.ts
+++ index.ts
@@ -10,11 +10,14 @@ type Machine = {
 }
 
 const CODE_EDITORS = [];
-// 1. create new machine
+// 1. create new machine and store the userId + machine Id + IP, increase the desied cap and return the Ip of the machine.
 app.post("/register", (req, res) => {
   const 
 });
-// 2. connect to the machine
-// 3. destroy machine
+
+// 2. Re-connect to the machine based on userId ( at a time, user can access only one machine at a time using his userId ).
+app.get("/editor/:editorId");
+// 3. destroy machine, if user wish to leave or if the machine was idle for more than 5 minutes.
+app.post("/kill/editor/:editorId");
 
 app.listen(3000, () => console.log("server started on PORT 3000");
