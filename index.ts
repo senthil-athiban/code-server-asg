dd8ced58948adfdcddec000b71baf4afa6cb8b2b
Senthil Athiban <senthilathiban0@gmail.com>
2025-03-25 23:37:21 +0530

    (fix) routes

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 
 const app = express();
 
@@ -7,17 +7,39 @@ type Machine = {
   ipAddress: string;
   isConnected: boolean;
   userId?: string;
+  isDisConnected: boolean; // check if the machine has been used or not
 }
 
 const CODE_EDITORS = [];
-// 1. create new machine and store the userId + machine Id + IP, increase the desied cap and return the Ip of the machine.
-app.post("/register", (req, res) => {
-  const 
+// 1. create new machine and store the userId + machine + IP, increase the desied cap and return the Ip of the machine.
+app.post("/register", async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  
+  // if user exists
+  const user = { email: "user@gmail.com", machine: {
+    ipAddress: "",
+    isConnected: false,
+    userId: "123"
+  }}
+
+  if(user) {
+    res.status(401).send({ message: "User already registered" })
+    return;
+  }
+
+  // get him a machine and store it in db;
+  const newUser = { email, password };
+  const savedUser = await newUser.save();
+
+  const machine = await aws.createMachine();
+  const { id, ipAddress } = machine;
+  // create machine record and save it along with user ref
 });
 
 // 2. Re-connect to the machine based on userId ( at a time, user can access only one machine at a time using his userId ).
-app.get("/editor/:editorId");
-// 3. destroy machine, if user wish to leave or if the machine was idle for more than 5 minutes.
-app.post("/kill/editor/:editorId");
+app.get("/editor/:editorId/connect");
 
-app.listen(3000, () => console.log("server started on PORT 3000");
+// 3. destroy machine, if user wish to leave or if the machine was idle for more than 5 minutes.
+app.post("/editor/:editorId/logout");
+
+app.listen(3000, () => console.log("server started on PORT 3000"));
